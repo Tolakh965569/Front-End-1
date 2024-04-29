4c1efbe72fb31ee8416d37c0919c9136eb4c4671
Long Tola <168409896+Tolakh965569@users.noreply.github.com>
2024-04-29 15:12:16 +0700

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Card.css";
+
+const Card = () => {
+    const style ={
+        color:"red"
+    }
+  return (
+  <>
+     <div className='card'>This is a Componnents</div>
+     <button className='Click'>Click Me</button>
+  </>
+ 
+  );
+  
+};
+
+export default Card
